43aca869fd55c2abb8a959b38e5d44dbba2f9d7f
mervecelikkkk <merve.celik56@outlook.com>
2023-03-05 22:55:20 +0300

    Add Missing Number

---
+++ missing-number/index.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+    const n = nums.length;
+    const expectedSum = (n * (n + 1)) / 2;
+    let actualSum = 0;
+  
+    for (let i = 0; i < nums.length; i++) {
+      actualSum = actualSum + nums[i]
+    }
+    
+    return expectedSum-actualSum
+  };
+  console.log(missingNumber([0,2,3]))
