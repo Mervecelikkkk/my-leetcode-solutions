e1d999c59ac76054a130b62f83b0442d7ef1d928
mervecelikkkk <merve.celik56@outlook.com>
2023-03-29 02:17:56 +0300

    Add Remove Element

---
+++ remove-element/index.js
@@ -0,0 +1,16 @@
+
+var removeElement=function removeElement(nums, val) {
+    let i = 0;
+    
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] !== val) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    
+    return i;
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
