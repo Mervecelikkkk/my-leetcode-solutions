8a98a8c268c738036dcd298d929fc4acdcb0f41b
mervecelikkkk <merve.celik56@outlook.com>
2023-03-29 02:10:21 +0300

    Add Two Sum II - Input array is sorted

---
+++ two-sum-II/index.js
@@ -0,0 +1,21 @@
+
+var twoSum = function(numbers, target) {
+    let left=0;
+    let right=numbers.length-1;
+    while(left<right) { 
+        
+        const sum=numbers[left]+numbers[right];
+        
+        if(sum===target)
+            return [left+1, right+1]
+        else if(sum<target) {
+            left++;
+        
+        } else 
+            right--;
+        
+    }
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O/1()
