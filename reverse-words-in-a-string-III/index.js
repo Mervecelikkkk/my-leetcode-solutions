3ff0a347e046d6c5b293322a96dbd5f7a9a451ff
mervecelikkkk <merve.celik56@outlook.com>
2023-04-10 23:06:45 +0300

    Add Reverse Words in a String III

---
+++ reverse-words-in-a-string-III/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    
+    s=s.split("").reverse().join("").split(" ").reverse().join(" ");
+    return s;
+  
+};
